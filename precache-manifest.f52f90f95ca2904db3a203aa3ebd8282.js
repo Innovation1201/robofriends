aabf68b9b35ccbe42103b5a08011519782e05b71
Samuel Smith <samuelsmith@Samuels-MBP-4.cablelynx.com>
2021-12-29 16:31:48 -0600

    Updates

---
+++ precache-manifest.f52f90f95ca2904db3a203aa3ebd8282.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "e45a6a62c3ec292d63db",
+    "url": "/robofriends/static/css/main.fed06f92.chunk.css"
+  },
+  {
+    "revision": "e45a6a62c3ec292d63db",
+    "url": "/robofriends/static/js/main.82867821.chunk.js"
+  },
+  {
+    "revision": "e273694afe77556ed6a0",
+    "url": "/robofriends/static/js/runtime~main.884587fe.js"
+  },
+  {
+    "revision": "daafcfd3ac486b7b4c48",
+    "url": "/robofriends/static/css/2.571b4589.chunk.css"
+  },
+  {
+    "revision": "daafcfd3ac486b7b4c48",
+    "url": "/robofriends/static/js/2.ae1c5755.chunk.js"
+  },
+  {
+    "revision": "510535223dcd279f3967",
+    "url": "/robofriends/static/js/3.e9d442d7.chunk.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "ad974f4d8ef8c7651b55bfdc01a59b4c",
+    "url": "/robofriends/index.html"
+  }
+];
